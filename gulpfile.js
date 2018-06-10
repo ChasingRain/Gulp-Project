33a110686c02fd84de5adc1103ed72aa6c425881
ChaingRain <36433203+ChaingRain@users.noreply.github.com>
2018-06-10 16:14:27 -0700

    Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 "use strict";
 
-var   gulp = require('gulp'),
-    concat = require("gulp-concat"),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify"),
-      sass = require("gulp-sass"),
-      maps = require("gulp-sourcemaps"),
-  cleanCSS = require('gulp-clean-css'),
-    image = require('gulp-image'),
-      del = require('del');
+var   gulp          = require('gulp'),
+    concat          = require("gulp-concat"),
+    rename          = require("gulp-rename"),
+    uglify          = require("gulp-uglify"),
+      sass          = require("gulp-sass"),
+      maps          = require("gulp-sourcemaps"),
+  cleanCSS          = require('gulp-clean-css'),
+    image           = require('gulp-image'),
+      del           = require('del'),
+      browserSync   = require('browser-sync').create();
+;
 
 gulp.task("jsConcat", ['cleanScripts'],  function(){
   return gulp.src(['js/circle/autogrow.js',
             'js/circle/circle.js',
             'js/global.js'])
   .pipe(concat("app.js"))
-  .pipe(gulp.dest('dist/scripts'));
+  .pipe(gulp.dest('dist/scripts'))
+  .pipe(browserSync.stream());
 });
 
 
@@ -25,7 +28,8 @@ gulp.task("scripts", ["jsConcat"], function(){
   .pipe(uglify())
   .pipe(rename("all.min.js"))
   .pipe(maps.write('./'))
-  .pipe(gulp.dest("dist/scripts"));
+  .pipe(gulp.dest("dist/scripts"))
+  .pipe(browserSync.stream());
 });
 
 gulp.task("concatCSS", function(){
@@ -34,6 +38,7 @@ gulp.task("concatCSS", function(){
   .pipe(sass())
   .pipe(maps.write('./'))
   .pipe(gulp.dest('dist/styles'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('styles', ['cleanStyles'],  function(){
@@ -43,14 +48,16 @@ gulp.task('styles', ['cleanStyles'],  function(){
   .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(rename("all.min.css"))
   .pipe(maps.write('./'))
-  .pipe(gulp.dest('dist/styles'));
+  .pipe(gulp.dest('dist/styles'))
+  .pipe(browserSync.stream());
 });
 
 
 gulp.task('image', ['cleanImages'],  function () {
   gulp.src('./images/*')
     .pipe(image())
-    .pipe(gulp.dest('./dist/content'));
+    .pipe(gulp.dest('./dist/content'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('clean', function () {
@@ -75,4 +82,13 @@ gulp.task('watch', function(){
 
 gulp.task('build', ['image','styles','scripts']);
 
-gulp.task('default',['build']);
+// Static Server + watching scss/html files
+gulp.task('default',['build'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./sass/*.scss", ['build']);
+    gulp.watch("./index.html").on('change', browserSync.reload);
+});
